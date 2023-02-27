dac1693724932aa66fd5fc71943ca147632cb368
berkanergil <berkannergil@gmail.com>
2023-02-27 11:42:34 +0200

    replaced files and route initialized

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
 app.listen(4000, () => {
-    console.log('listening on 4000');
+  console.log("listening on 4000");
 });
